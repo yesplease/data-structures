8d1523a85a93516176421c3331fd633811e691ab
yesplease <bradley.heinz@gmail.com>
2015-02-06 12:36:59 -0800

    Finished most of the tree data structure still working on contains

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,10 +1,10 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
-
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
@@ -15,10 +15,39 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  //push to the Tree's children
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
+
+  if (this.value === target){
+      answer = true;
+      console.log("value equals target");
+    }
+  var answer = _.each(this.children, function(element, index, collection){
+    console.log("This is the value " , element.value);
+    console.log("This is the target ", target);
+    if (element.value === target){
+      return true;
+      console.log("value equals target");
+    }
+    console.log("this dot children ", element);
+    return element.contains(target);
+    return false;
+  });
+
+  console.log(answer);
+  return answer;
+  //does the value equal the target? y/n
+  //if y then return true
+  //if n then check if this.children.length > 0 y/n
+  //if y for each of the children call contains on this.children[something]
+  //if n then return false
+
+
+
+
 
 };
 
@@ -26,3 +55,23 @@ treeMethods.contains = function(target){
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+
+  // var answer = false;
+  // if(this.value === target){
+  //   answer = true;
+  // } else {
+  //   _.each(this.children, function(element, index, collection){
+  //     if (this.children.length !== 0){
+  //       contains(this.children[])
+  //     }
+  //     }
+  //     // _.each(element, function(value, key, collection){
+  //     //   if(this.value === target){
+  //     //     answer = true;
+  //     //   }
+  //     // })
+  //   })
+  // }
+  // return answer;
