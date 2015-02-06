c9f9095aaf5ea6c512bc6f2661ab95b5ac8867ef
yesplease <bradley.heinz@gmail.com>
2015-02-05 17:45:53 -0800

    Finished some of the Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,15 +3,48 @@ var LinkedList = function(){
   list.head = null;
   list.tail = null;
 
+
   list.addToTail = function(value){
-  };
+
+    console.log('this is the value: ', value);
+    var newNode = Node(value);
+    // newNode.next = list.head;
+
+    if (this.head === null){
+      this.head = newNode;
+      this.tail = newNode
+    };
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+  }
 
   list.removeHead = function(){
+    //this.head = this.head.next;
+    //if this.head.next === null:
+    //set this.head to null
+    if(this.head.next === null){
+      this.head = null;
+    };
+    var result = this.head.value;
+    this.head = this.head.next;
+    return result;
   };
 
   list.contains = function(target){
+    if(){
+      return false;
+    } else {
+      return true;
+    }
+    //look at head to see if it matches the target
+    //if it does then return true
+    //move head to tail
+    //if at terminator
+    //move head to tail
   };
 
+  console.log(list);
   return list;
 };
 
@@ -27,3 +60,25 @@ var Node = function(value){
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+// //first instance
+// {
+//   list.a = {
+//     node.value = 'a';
+//     node.next = null;
+//   }
+//   list.head:'a'
+//   list.tail:'a'
+// }
+// //second instance (adding b):
+// {
+//   list.a = {
+//     node.value = 'a';
+//     node.next = b;
+//   },
+//   list.b = {
+//     node.value = 'b';
+//     node.next = null;
+//   }
+//   list.head:'a'
+//   list.tail:'b'
+// }
