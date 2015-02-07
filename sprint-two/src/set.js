d89f39d5631ccdfb9e703baf023ed2c387698639
yesplease <bradley.heinz@gmail.com>
2015-02-06 21:29:30 -0800

    Finished set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,30 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage.push(item);
 };
 
 setPrototype.contains = function(item){
+  for(var i = 0; i < this._storage.length; i++){
+    if(item === this._storage[i]){
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item){
+  for(var i = 0; i < this._storage.length; i++){
+    if (item === this._storage[i]){
+      this._storage.splice(i,1);
+    }
+  }
 };
 
 /*
