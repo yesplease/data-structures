577ebc6bdb86c52e1f581eefa205f069a6cb91b8
yesplease <bradley.heinz@gmail.com>
2015-02-07 11:19:30 -0800

    finished insert for binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,48 @@
 var BinarySearchTree = function(value){
+  var newNode = Object.create(binaryTreeMethods);
+  newNode.value = value;
+  newNode.left = null;
+  newNode.right = null;
+  return newNode;
 };
+
+var binaryTreeMethods = {};
+
+binaryTreeMethods.insert = function(newValue){
+
+
+  var currentRoot = this;
+
+  while(currentRoot !== null){
+    if(currentRoot.value === newValue){
+      return;
+    }
+    if(currentRoot.value < newValue){
+      if(currentRoot.right === null){
+        currentRoot.right = BinarySearchTree(newValue);
+      }
+      currentRoot = currentRoot.right;
+    }
+    if(currentRoot.value > newValue){
+      if(currentRoot.left === null){
+        currentRoot.left = BinarySearchTree(newValue);
+      }
+      currentRoot = currentRoot.left;
+    }
+  }
+};
+
+binaryTreeMethods.contains = function(target){
+
+
+
+};
+
+binaryTreeMethods.depthFirstLog = function(){  //note: check argument
+
+
+};
+
 
 
 /*
