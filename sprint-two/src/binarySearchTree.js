620e5489827f024a12e60ad28c41df9a38a970a9
yesplease <bradley.heinz@gmail.com>
2015-02-07 16:44:13 -0800

    Finished all of sprint 2!

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -10,7 +10,6 @@ var binaryTreeMethods = {};
 
 binaryTreeMethods.insert = function(newValue){
 
-
   var currentRoot = this;
 
   while(currentRoot !== null){
@@ -33,15 +32,54 @@ binaryTreeMethods.insert = function(newValue){
 };
 
 binaryTreeMethods.contains = function(target){
-
-
-
+  var currentRoot = this;
+   while (currentRoot !== null){
+    if (currentRoot.value === target){
+      return true;
+    }
+    if (currentRoot.value < target){
+      if(currentRoot.right === null) {
+        return false;
+      }
+      currentRoot = currentRoot.right;
+    }
+    if (currentRoot.value > target) {
+      if (currentRoot.left === null){
+        return false;
+      }
+      currentRoot = currentRoot.left;
+    }
+   }
 };
 
-binaryTreeMethods.depthFirstLog = function(){  //note: check argument
+binaryTreeMethods.depthFirstLog = function(callback){  //note: check argument
+  //store the current context in this as 'currentRoot'
+  var currentRoot = this;
+  // console.log('This is the currentRoot: ', currentRoot);
+  var callbackArr = [];
+  // console.log('This is the callbackArr: ', callbackArr);
+  var answer = [];
+  //at some point at each value for the current root we need to call the callback
+  //callback(currentRoot.value)
 
+  var depthRecursiveSearch = function(node){
+    callbackArr.push(node.value)
+    if(node.left !== null){
+      depthRecursiveSearch(node.left);
+    }
+    if(node.right !== null){
+      depthRecursiveSearch(node.right);
+    }
+  };
 
-};
+  depthRecursiveSearch(currentRoot);
+
+  for(var i = 0; i < callbackArr.length; i++){
+    answer.push(callback(callbackArr[i]));
+  };
+  return answer;
+}
+
 
 
 
